3e3404824396102a8ae393d07b446c4de7cbff88
CuzImBisonratte <cuzimbisonratte@gmail.com>
2023-05-16 00:02:32 +0200

    Add dashboard and survey page

---
+++ admin/surveys.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administration | uTool</title>
+    <link rel="stylesheet" href="/res/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="/res/fontawesome/css/solid.min.css">
+    <link rel="stylesheet" href="/res/css/fonts.css">
+    <link rel="stylesheet" href="/res/css/main.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <div class="menu">
+            <div class="Dashboard" onclick="location.assign('./');"><span><i class="fas fa-tachometer-alt"></i> Dashboard</span></div>
+            <div class="surveys" onclick="location.assign('./surveys.php');"><span><i class="fas fa-poll"></i> Surveys</span></div>
+            <div class="users" onclick="location.assign('./users.php');"><span><i class="fas fa-users"></i> Users</span></div>
+            <div class="settings" onclick="location.assign('./settings.php');"><span><i class="fas fa-cog"></i> Settings</span></div>
+        </div>
+        <div class="account"></div>
+    </nav>
+    <main>
+        <div class="accent-color"></div>
+        <div class="page-title">
+            Surveys
+            <div class="add-survey-button"><i class="fas fa-plus"></i> Add survey</div>
+        </div>
+        <div class="survey-list">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, beatae asperiores nulla minima adipisci facilis?
+        </div>
+    </main>
+</body>
+
+</html>
