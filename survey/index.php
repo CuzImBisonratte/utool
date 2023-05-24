0d7f1444e9f6f7f75ad721155cc7caec5f40c1dd
CuzImBisonratte <cuzimbisonratte@gmail.com>
2023-05-24 23:08:11 +0200

    Fix title

--- survey/index.php
+++ survey/index.php
@@ -42,7 +42,7 @@ $questions = $questions_sorted;
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administration | uTool</title>
+    <title><?= $survey["title"] ?> | uTool</title>
     <link rel="stylesheet" href="/res/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="/res/fontawesome/css/solid.min.css">
     <link rel="stylesheet" href="/res/css/fonts.css">
